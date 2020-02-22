41ae52bd73e28c9fac2b627f0553e46e00c2cba8
Merlin Carson <mpc6@.pdx.edu>
2020-02-21 16:19:14 -0800

    removed training

--- train.py
+++ train.py
@@ -40,27 +40,27 @@ def main():
     print('Loading dataset ...\n')
     dataset_train = Dataset(train=True)
     dataset_val = Dataset(train=False)
-    loader_train = DataLoader(dataset=dataset_train, num_workers=os.cpu_count(), batch_size=opt.batchSize*len(device_ids), shuffle=True)
+    loader_train = DataLoader(dataset=dataset_train, num_workers=0, batch_size=opt.batchSize, shuffle=True)
     print("# of training samples: %d\n" % int(len(dataset_train)))
     print("# of validation samples: %d\n" % int(len(dataset_val)))
     # Build model
-    net = DnCNN(channels=1, num_of_layers=opt.num_of_layers)
-    net.apply(weights_init_kaiming)
-    criterion = nn.MSELoss(reduction='sum')
+ #   net = DnCNN(channels=1, num_of_layers=opt.num_of_layers)
+ #   net.apply(weights_init_kaiming)
+ #   criterion = nn.MSELoss(reduction='sum')
     # Move to GPU
     #device_ids = [0]
-    model = nn.DataParallel(net, device_ids=device_ids).cuda()
+ #   model = nn.DataParallel(net, device_ids=device_ids).cuda()
     #print(device_ids)
     #model = nn.parallel.DistributedDataParallel(net, device_ids=device_ids).cuda()
     #model = nn.parallel.DistributedDataParallel(net).cuda()
-    criterion.cuda()
+ #   criterion.cuda()
     # Optimizer
-    optimizer = optim.Adam(model.parameters(), lr=opt.lr)
+ #   optimizer = optim.Adam(model.parameters(), lr=opt.lr)
     #scheduler = ReduceLROnPlateau(optimizer, 'min')
     # training
-    writer = SummaryWriter(opt.outf)
+ #   writer = SummaryWriter(opt.outf)
     step = 0
-    noiseL_B=[0,55] # ingnored when opt.mode=='S'
+ #   noiseL_B=[0,55] # ingnored when opt.mode=='S'
     
     for epoch in range(opt.epochs):
         if epoch < opt.milestone:
@@ -68,16 +68,16 @@ def main():
         else:
             lr = opt.lr / 10.
         # set learning rate
-        for param_group in optimizer.param_groups:
-            param_group["lr"] = lr
+#        for param_group in optimizer.param_groups:
+#            param_group["lr"] = lr
         # train
         print(f'Starting epoch {epoch+1} with lr={lr}')
         #for i, data in enumerate(loader_train, 0):
         for data in tqdm(loader_train):
             # training step
-            model.train()
-            model.zero_grad()
-            optimizer.zero_grad()
+ #           model.train()
+ #           model.zero_grad()
+ #           optimizer.zero_grad()
             img_train = data
             if opt.mode == 'S':
                 noise = torch.FloatTensor(img_train.size()).normal_(mean=0, std=opt.noiseL/255.)
@@ -89,13 +89,13 @@ def main():
                     noise[n,:,:,:] = torch.FloatTensor(sizeN).normal_(mean=0, std=stdN[n]/255.)
             imgn_train = img_train + noise
             img_train, imgn_train = Variable(img_train.cuda()), Variable(imgn_train.cuda())
-            noise = Variable(noise.cuda())
-            out_train = model(imgn_train)
-            loss = criterion(out_train, noise) / (imgn_train.size()[0]*2)
-            loss.backward()
-            optimizer.step()
+ #           noise = Variable(noise.cuda())
+ #           out_train = model(imgn_train)
+ #           loss = criterion(out_train, noise) / (imgn_train.size()[0]*2)
+ #           loss.backward()
+ #           optimizer.step()
             # results
-            model.eval()
+ #           model.eval()
             #out_train = torch.clamp(imgn_train-model(imgn_train), 0., 1.)
             #psnr_train = batch_PSNR(out_train, img_train, 1.)
             #print("[epoch %d][%d/%d] loss: %.4f PSNR_train: %.4f" %
@@ -107,7 +107,7 @@ def main():
             #    writer.add_scalar('PSNR on training data', psnr_train, step)
             step += 1
         ## the end of each epoch
-        model.eval()
+ #       model.eval()
         # validate
         psnr_val = 0
         #1val_loss = 0
@@ -115,27 +115,27 @@ def main():
             img_val = torch.unsqueeze(dataset_val[k], 0)
             noise = torch.FloatTensor(img_val.size()).normal_(mean=0, std=opt.val_noiseL/255.)
             imgn_val = img_val + noise
-            with torch.no_grad():
-                img_val, imgn_val = Variable(img_val.cuda()), Variable(imgn_val.cuda())
-                out_val = torch.clamp(imgn_val-model(imgn_val), 0., 1.)
+ #           with torch.no_grad():
+ #               img_val, imgn_val = Variable(img_val.cuda()), Variable(imgn_val.cuda())
+ #               out_val = torch.clamp(imgn_val-model(imgn_val), 0., 1.)
         #        val_loss += criterion(out_train, noise) / (imgn_val.size()[0]*2)
-                psnr_val += batch_PSNR(out_val, img_val, 1.)
-        psnr_val /= len(dataset_val)
+ #               psnr_val += batch_PSNR(out_val, img_val, 1.)
+ #       psnr_val /= len(dataset_val)
         #val_loss /= len(dataset_val)
-        print(f"\nPSNR_val: {psnr_val:.4f}")
+ #       print(f"\nPSNR_val: {psnr_val:.4f}")
         #print(f"\nval_loss: {val_loss:.4f}")
-        writer.add_scalar('PSNR on validation data', psnr_val, epoch)
+ #       writer.add_scalar('PSNR on validation data', psnr_val, epoch)
         #writer.add_scalar('val_loss', val_loss.item(), epoch)
         # log the images
-        out_train = torch.clamp(imgn_train-model(imgn_train), 0., 1.)
-        Img = utils.make_grid(img_train.data, nrow=8, normalize=True, scale_each=True)
-        Imgn = utils.make_grid(imgn_train.data, nrow=8, normalize=True, scale_each=True)
-        Irecon = utils.make_grid(out_train.data, nrow=8, normalize=True, scale_each=True)
-        writer.add_image('clean image', Img, epoch)
-        writer.add_image('noisy image', Imgn, epoch)
-        writer.add_image('reconstructed image', Irecon, epoch)
+ #       out_train = torch.clamp(imgn_train-model(imgn_train), 0., 1.)
+ #       Img = utils.make_grid(img_train.data, nrow=8, normalize=True, scale_each=True)
+ #       Imgn = utils.make_grid(imgn_train.data, nrow=8, normalize=True, scale_each=True)
+ #       Irecon = utils.make_grid(out_train.data, nrow=8, normalize=True, scale_each=True)
+ #       writer.add_image('clean image', Img, epoch)
+ #       writer.add_image('noisy image', Imgn, epoch)
+ #       writer.add_image('reconstructed image', Irecon, epoch)
         # save model
-        torch.save(model.state_dict(), os.path.join(opt.outf, 'net.pth'))
+ #       torch.save(model.state_dict(), os.path.join(opt.outf, 'net.pth'))
 
 if __name__ == "__main__":
     if opt.preprocess:
