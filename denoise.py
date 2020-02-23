efcd858e2da5fa7eb65e1f8cc08eec0e48fe0041
Merlin Carson <mpc6@.pdx.edu>
2020-02-23 15:28:13 -0800

    finished denoising script

--- denoise.py
+++ denoise.py
@@ -14,6 +14,7 @@ def main():
 
     parser = argparse. ArgumentParser(description='Image Denoising')
     parser.add_argument('--img_dir', type=str, default='data/Set12', help='location of files to denoise')
+    parser.add_argument('--out_dir', type=str, default='output', help='location to save output images')
     parser.add_argument('--img', type=str, help='location of a file to denoise')
     parser.add_argument('--noise_level', type=float, default=25.0, help='noise level for training')
     parser.add_argument('--model_name', type=str, default='logs/best_model.pt', help='location of model file')
@@ -33,8 +34,8 @@ def main():
     model = torch.load(args.model_name)
     model.eval() 
 
-    if args.img_dir is not None:
-        os.makedirs(os.path.join(args.img_dir, 'denoised'), exist_ok=True)
+    # make sure output directory exists
+    os.makedirs(args.out_dir, exist_ok=True)
 
     test_psnr = 0
     num_test_files = 0
@@ -42,37 +43,35 @@ def main():
         print(f'Denoising {f}')
 
         # prepare clean image
-        img = cv.imread(f).astype(np.float32)
-        clean_img = img[:,:,0]/255
+        img = cv.imread(f).astype(np.float32)[:,:,:1]
+        clean_img =  img/255
         clean_img = np.reshape(clean_img, (1,1,clean_img.shape[0], clean_img.shape[1]))
-        clean_img = Variable(torch.Tensor(clean_img).cuda())
+        clean_img = torch.FloatTensor(clean_img)
 
         # prepare noisy image
         noise = torch.FloatTensor(clean_img.size()).normal_(mean=0, std=noise_level)
         noisy_img = Variable((clean_img + noise).cuda())
 
         with torch.no_grad():
-            denoised_img = torch.clap(noisy_img - model(noisy_img), 0.0, 1.0)
+            denoised_img = torch.clamp(noisy_img - model(noisy_img), 0.0, 1.0)
             
         # save images
-        file_dir_name = os.path.dirname(f)
         file_name = os.path.basename(f)
-
-        denoised_img = np.array(denoised_img.cpu().data.numpy.reshape((denoised_img.shape[2], denoised_img.shape[3], denoised_img.shape[1]))).astype(np.float32) 
+        denoised_img = denoised_img.cpu().data.numpy().reshape((denoised_img.shape[2], denoised_img.shape[3], denoised_img.shape[1])).astype(np.float32) 
         denoised_img *= 255     # undo normalization
-        cv.imwrite(img=denoised_img.clip(0.0, 255.0).astype('uint8'), filename=os.path.join(file_dir_name, file_name.replace('.png', '-denoised.png')))
+        cv.imwrite(img=denoised_img.clip(0.0, 255.0).astype('uint8'), filename=os.path.join(args.out_dir, file_name.replace('.png', '-denoised.png')))
 
-        noisy_img = np.array(noisy_img.cpu().data.numpy.reshape((denoised_img.shape[2], denoised_img.shape[1], denoised_img.shape[0])))
+        noisy_img = np.array(noisy_img.cpu().data.numpy().reshape((noisy_img.shape[2], noisy_img.shape[3], noisy_img.shape[1]))).astype(np.float32)
         noisy_img *= 255        # undo normalization
-        cv.imwrite(img=noisy_img.clip(0.0, 255.0).astype('uint8'), filename=os.path.join(file_dir_name, file_name.replace('.png', '-noisy.png')))
+        cv.imwrite(img=noisy_img.clip(0.0, 255.0).astype('uint8'), filename=os.path.join(args.out_dir, file_name.replace('.png', '-noisy.png')))
 
         pnsr = psnr(img, denoised_img, data_range=255)
         print(f'PNSR of {f}: {pnsr}')
 
-        test_pnsr += pnsr
+        test_psnr += pnsr
         num_test_files += 1
 
-    print(f'Average PNSR of testset is {test_pnsr/num_test_files}')
+    print(f'Average PNSR of testset is {test_psnr/num_test_files}')
 
     return 0
 
