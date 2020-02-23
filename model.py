e8c24aca422f5869a26b7029cd02c0b12084514e
Merlin Carson <mpc6@.pdx.edu>
2020-02-23 15:38:09 -0800

    cleaned up weight init

--- model.py
+++ model.py
@@ -2,6 +2,16 @@ import torch
 import torch.nn as nn
 import math
 
+
+def init_weights(model):
+    for layer in model:
+        if isinstance(layer, nn.Conv2d):
+            nn.init.kaiming_normal_(layer.weight.data, a=0, mode='fan_in')
+        if isinstance(layer, nn.BatchNorm2d):
+            layer.weight.data.normal_(mean=0, std=math.sqrt(2./9./64.)).clamp_(-0.025,0.025)
+            nn.init.constant_(layer.bias.data, 0.0)
+
+
 class DnCNN(nn.Module):
     def __init__(self, num_channels, patch_size, num_layers, kernel_size, stride, num_filters):
         super(DnCNN, self).__init__()
@@ -19,18 +29,10 @@ class DnCNN(nn.Module):
             self.layers.append(nn.ReLU(inplace=True))
         self.layers.append(nn.Conv2d(in_channels=num_filters, out_channels=num_channels, kernel_size=kernel_size, padding=padding, bias=False))
         self.model = nn.ModuleList(self.layers)
-        self.init_weights()
+        init_weights(self.model)
         # create sequential model
         #self.model = nn.Sequential(*self.layers)
 
-    def init_weights(self):
-        for layer in self.model:
-            if isinstance(layer, nn.Conv2d):
-                nn.init.kaiming_normal_(layer.weight.data, a=0, mode='fan_in')
-            if isinstance(layer, nn.BatchNorm2d):
-                layer.weight.data.normal_(mean=0, std=math.sqrt(2./9./64.)).clamp_(-0.025,0.025)
-                nn.init.constant_(layer.bias.data, 0.0)
-                
     def forward(self, x):
         #preds = self.model(x)
         for layer in self.model:
@@ -53,17 +55,9 @@ class DnCNN_Res(nn.Module):
             self.layers.append(ResBlock( num_filters, kernel_size, padding))
         self.layers.append(nn.Conv2d(in_channels=num_filters, out_channels=num_channels, kernel_size=kernel_size, padding=padding, bias=False))
         self.model = nn.ModuleList(self.layers)
-        self.init_weights()
+        init_weights(self.model)
         # create sequential model
         #self.model = nn.Sequential(*self.layers)
-
-    def init_weights(self):
-        for layer in self.model:
-            if isinstance(layer, nn.Conv2d):
-                nn.init.kaiming_normal_(layer.weight.data, a=0, mode='fan_in')
-            if isinstance(layer, nn.BatchNorm2d):
-                layer.weight.data.normal_(mean=0, std=math.sqrt(2./9./64.)).clamp_(-0.025,0.025)
-                nn.init.constant_(layer.bias.data, 0.0)
                 
     def forward(self, x):
         #preds = self.model(x)
@@ -75,37 +69,17 @@ class DnCNN_Res(nn.Module):
 class ResBlock(nn.Module):
     def __init__(self, num_filters, kernel_size, padding):
         super(ResBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=num_filters, out_channels=num_filters, kernel_size=kernel_size, padding=padding, bias=False)
-        nn.init.kaiming_normal_(self.conv1.weight.data, a=0, mode='fan_in')
-        self.bn1 = nn.BatchNorm2d(num_filters)
-        self.bn1.weight.data.normal_(mean=0, std=math.sqrt(2./9./64.)).clamp_(-0.025,0.025)
-        nn.init.constant_(self.bn1.bias.data, 0.0)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(in_channels=num_filters, out_channels=num_filters, kernel_size=kernel_size, padding=padding, bias=False)
-        nn.init.kaiming_normal_(self.conv2.weight.data, a=0, mode='fan_in')
-        self.bn2 = nn.BatchNorm2d(num_filters)
-        self.bn2.weight.data.normal_(mean=0, std=math.sqrt(2./9./64.)).clamp_(-0.025,0.025)
-        nn.init.constant_(self.bn2.bias.data, 0.0)
-        #self.init_weights()
+        self.layers = []
+        self.layers.append(nn.Conv2d(in_channels=num_filters, out_channels=num_filters, kernel_size=kernel_size, padding=padding, bias=False))
+        self.layers.append(nn.BatchNorm2d(num_filters))
+        self.layers.append(nn.ReLU(inplace=True))
+        self.layers.append(nn.Conv2d(in_channels=num_filters, out_channels=num_filters, kernel_size=kernel_size, padding=padding, bias=False))
+        self.layers.append(nn.BatchNorm2d(num_filters))
+        self.model = nn.ModuleList(self.layers)
+        init_weights(self.model)
 
-    def init_weights(self):
-        for layer in self.model:
-            if isinstance(layer, nn.Conv2d):
-                nn.init.kaiming_normal_(layer.weight.data, a=0, mode='fan_in')
-            if isinstance(layer, nn.BatchNorm2d):
-                layer.weight.data.normal_(mean=0, std=math.sqrt(2./9./64.)).clamp_(-0.025,0.025)
-                nn.init.constant_(layer.bias.data, 0.0)
-                
     def forward(self, x):
         identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(x)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        out += identity
-
-        return self.relu(out)
+        for layer in self.model:
+            x = layer(x)
+        return nn.ReLU(inplace=True)(identity+x)
