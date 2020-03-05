95ccee826a46c2a3f7aa3d26277a3cd172aa6dc1
Merlin Carson <merlin.carson@biamp.com>
2020-03-04 20:44:58 -0800

    using clean_img as target, switched to Adadelta

--- train.py
+++ train.py
@@ -102,18 +102,18 @@ def main():
     print(f'Number of training examples: {len(train_data)}')
     print(f'Number of validation examples: {len(val_data)}')
 
+    # detect gpus and setup environment variables
+    device_ids = setup_gpus()
+    print(f'Cuda devices found: {[torch.cuda.get_device_name(i) for i in device_ids]}')
+
     # create batched data loaders for model
-    train_loader = DataLoader(dataset=train_data, num_workers=os.cpu_count(), batch_size=args.batch_size, shuffle=True)
-    val_loader = DataLoader(dataset=val_data, num_workers=os.cpu_count(), batch_size=args.batch_size, shuffle=False)
+    train_loader = DataLoader(dataset=train_data, num_workers=os.cpu_count(), batch_size=args.batch_size*len(device_ids), shuffle=True)
+    val_loader = DataLoader(dataset=val_data, num_workers=os.cpu_count(), batch_size=args.batch_size*len(device_ids), shuffle=False)
 
     # input shape for each example to network, NOTE: channels first
     num_channels, patch_height, patch_width = train_data.shape()
 
     print(f'Input shape to model forward will be: ({args.batch_size}, {num_channels}, {patch_height}, {patch_width})')
-
-    # detect gpus and setup environment variables
-    device_ids = setup_gpus()
-    print(f'Cuda devices found: {[torch.cuda.get_device_name(i) for i in device_ids]}')
 
     # create model
     model = DnCNN_Res(num_channels=num_channels, patch_size=patch_height,  num_layers=args.num_layers, \
@@ -124,7 +124,8 @@ def main():
 
     # setup loss and optimizer
     criterion = torch.nn.MSELoss(reduction='sum').cuda()
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    #optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    optimizer = torch.optim.Adadelta(model.parameters())
     scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=args.lr_decay)
 
     # data struct to track training and validation losses per epoch
@@ -136,10 +137,11 @@ def main():
     writer = SummaryWriter(args.log_dir)
 
     # schedulers
-#    scheduler = ReduceLROnPlateau(optimizer, 'min', verbose=True, patience=10)
+    #scheduler = ReduceLROnPlateau(optimizer, 'min', verbose=True, patience=10)
 
     # Main training loop
     best_val_loss = 999
+    best_psnr = 0
     for epoch in range(args.epochs):
         print(f'Starting epoch {epoch+1} with learning rate {optimizer.param_groups[0]["lr"]}')
         model.train()
@@ -157,14 +159,14 @@ def main():
             # pack input and target it into torch variable
             clean_imgs = Variable(data.cuda()) 
             noisy_imgs = Variable((data + noise).cuda()) 
-            noise = Variable(noise.cuda())
+            #noise = Variable(noise.cuda())
 
             # make predictions
-            preds = model(noisy_imgs)
+            preds = model(torch.clamp(noisy_imgs, 0.0, 1.0))
 
             # calculate loss
-            loss = criterion(preds, noise)/(2*noisy_imgs.size()[0])
-            epoch_train_loss += loss.item()
+            loss = criterion(preds, clean_imgs)/(2*noisy_imgs.size()[0])
+            epoch_train_loss += loss.detach()
 
             # backprop
             loss.backward()
@@ -186,17 +188,17 @@ def main():
                 # pack input and target it into torch variable
                 clean_imgs = Variable(data.cuda()) 
                 noisy_imgs = Variable((data + noise).cuda()) 
-                noise = Variable(noise.cuda())
+                #noise = Variable(noise.cuda())
 
                 # make predictions
-                preds = model(noisy_imgs)
+                preds = model(torch.clamp(noisy_imgs, 0.0, 1.0))
 
                 # calculate loss
-                val_loss = criterion(preds, noise)/noisy_imgs.size()[0]
-                epoch_val_loss += val_loss.item()
+                val_loss = criterion(preds, clean_imgs)/(2*noisy_imgs.size()[0])
+                epoch_val_loss += val_loss.detach()
 
                 # calculate PSNR 
-                denoised_imgs = torch.clamp(noisy_imgs-preds, 0.0, 1.0)
+                denoised_imgs = torch.clamp(preds, 0.0, 1.0)
                 epoch_val_psnr += psnr_of_batch(clean_imgs, denoised_imgs)
 
 
@@ -207,17 +209,10 @@ def main():
         epoch_val_loss /= val_steps
         epoch_val_psnr /= val_steps
 
-        # save if best model
-        if epoch_val_loss < best_val_loss:
-            print('Saving best model')
-            best_val_loss = epoch_val_loss
-            torch.save(model, os.path.join(args.log_dir, 'best_model.pt'))
-            pickle.dump(history, open(os.path.join(args.log_dir, 'best_model.npy'), 'wb'))
-
         # reduce learning rate if validation has leveled off
         #scheduler.step(epoch_val_loss)
         # learning rate exponential decay 
-        scheduler.step()
+        #scheduler.step()
 
         # save epoch stats
         history['train'].append(epoch_train_loss)
@@ -229,6 +224,15 @@ def main():
         writer.add_scalar('loss', epoch_train_loss, epoch)
         writer.add_scalar('val', epoch_val_loss, epoch)
         writer.add_scalar('PSNR', epoch_val_psnr, epoch)
+
+        # save if best model
+        #if epoch_val_loss < best_val_loss:
+        if epoch_val_psnr < best_psnr:
+            print('Saving best model')
+            best_psnr = epoch_val_psnr
+            #best_val_loss = epoch_val_loss
+            torch.save(model, os.path.join(args.log_dir, 'best_model.pt'))
+            pickle.dump(history, open(os.path.join(args.log_dir, 'best_model.npy'), 'wb'))
 
         # test model and save results 
         if epoch % 5 == 0:
